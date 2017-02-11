286b40af74741950b9cbf9051164eb29f8384f86
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-02-11 12:58:29 -0800

    Added total config

--- src/main/java/io/cdep/CMakeGenerator.java
+++ src/main/java/io/cdep/CMakeGenerator.java
@@ -107,7 +107,7 @@ class CMakeGenerator {
             generateFinderExpression(0, findFunction, findFunction, sb);
             // TODO: If two artifact IDs conflict then generate a Find*.cmake that emits an error
         }
-        File file = new File(environment.modulesFolder, "Findcdep-dependencies.cmake");
+        File file = new File(environment.modulesFolder, "Findcdep.cmake");
         writeTextToFile(file, sb.toString());
     }
 
