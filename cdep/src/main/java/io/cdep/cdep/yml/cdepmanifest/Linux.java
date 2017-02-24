08e933c28f11e11beabfe41bb725325f7bbe5a65
Jomo Fisher <jomof@google.com>
2017-02-24 14:33:03 -0800

    packageArtifacts

--- cdep/src/main/java/io/cdep/cdep/yml/cdepmanifest/Linux.java
+++ cdep/src/main/java/io/cdep/cdep/yml/cdepmanifest/Linux.java
@@ -2,6 +2,7 @@ package io.cdep.cdep.yml.cdepmanifest;
 
 @SuppressWarnings("unused")
 public class Linux {
+  final public Archive archives[] = null;
   final public String file = null;
   final public String sha256 = null;
   final public String include;
