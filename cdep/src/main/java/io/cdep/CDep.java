30490632ac09758bfec48b6d5bb61e4e29ed1e7e
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-02-27 17:45:31 -0800

    Transitive dependencies

--- cdep/src/main/java/io/cdep/CDep.java
+++ cdep/src/main/java/io/cdep/CDep.java
@@ -55,7 +55,7 @@ public class CDep {
             new CDep(System.out).go(args);
         } catch (Throwable e) {
             e.printStackTrace();
-            return 1;
+            System.exit(1);
         }
         return 0;
     }
