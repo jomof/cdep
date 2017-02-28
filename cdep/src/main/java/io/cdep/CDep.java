6f871c1dfe1f540da52f3c4f06011d9111705850
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-02-27 17:15:27 -0800

    Transitive dependencies

--- cdep/src/main/java/io/cdep/CDep.java
+++ cdep/src/main/java/io/cdep/CDep.java
@@ -49,9 +49,15 @@ public class CDep {
         this.out = out;
     }
 
-    public static void main(String[] args)
+    public static int main(String[] args)
         throws IOException, URISyntaxException, NoSuchAlgorithmException {
-        new CDep(System.out).go(args);
+        try {
+            new CDep(System.out).go(args);
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return 1;
+        }
+        return 0;
     }
 
     void go(String[] args) throws IOException, URISyntaxException, NoSuchAlgorithmException {
