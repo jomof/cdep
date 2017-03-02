2bca1cf1d346dfd885d0722d8de71d1de7a6bb43
Jomo Fisher <jomof@google.com>
2017-03-02 13:06:18 -0800

    Report known ABIs when there is no match

--- cdep/src/main/java/io/cdep/cdep/generator/AndroidAbi.java
+++ cdep/src/main/java/io/cdep/cdep/generator/AndroidAbi.java
@@ -4,31 +4,18 @@ package io.cdep.cdep.generator;
  * Enum of valid ABI you can specify for NDK.
  */
 public enum AndroidAbi {
-  ARMEABI("armeabi", "arm"),
-  ARMEABI_V7A("armeabi-v7a", "arm"),
-  ARM64_V8A("arm64-v8a", "arm64"),
-  X86("x86", "x86"),
-  X86_64("x86_64", "x86_64");
+  ARMEABI("armeabi"),
+  ARMEABI_V7A("armeabi-v7a"),
+  ARM64_V8A("arm64-v8a"),
+  X86("x86"),
+  X86_64("x86_64"),
+  MIPS("mips"),
+  MIPS64("mips64");
 
   private final String name;
-  private final String architecture;
 
-  private AndroidAbi(String name, String architecture) {
+  private AndroidAbi(String name) {
     this.name = name;
-    this.architecture = architecture;
-
-  }
-
-  /**
-   * Returns the ABI Enum with the specified name.
-   */
-  public static AndroidAbi getByName(String name) {
-    for (AndroidAbi abi : values()) {
-      if (abi.name.equals(name)) {
-        return abi;
-      }
-    }
-    throw new RuntimeException(name);
   }
 
   /**
@@ -36,13 +23,6 @@ public enum AndroidAbi {
    */
   public String getName() {
     return name;
-  }
-
-  /**
-   * Returns the CPU architecture like "arm".
-   */
-  public String getArchitecture() {
-    return architecture;
   }
 
   /**
