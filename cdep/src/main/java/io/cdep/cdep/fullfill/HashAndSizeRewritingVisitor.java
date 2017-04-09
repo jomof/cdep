7e5c86feee6aeb2556aea3b6d72e0ea91c062347
Jomo Fisher (desktop) <jomofisher@gmail.com>
2017-04-08 17:10:33 -0700

    Fullfillment

---
+++ cdep/src/main/java/io/cdep/cdep/fullfill/HashAndSizeRewritingVisitor.java
@@ -0,0 +1,32 @@
+package io.cdep.cdep.fullfill;
+
+import io.cdep.cdep.utils.HashUtils;
+import io.cdep.cdep.yml.cdepmanifest.Archive;
+import io.cdep.cdep.yml.cdepmanifest.CDepManifestYmlRewritingVisitor;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+public class HashAndSizeRewritingVisitor extends CDepManifestYmlRewritingVisitor {
+  private final File layoutFolder;
+
+  HashAndSizeRewritingVisitor(File layoutFolder) {
+    this.layoutFolder = layoutFolder;
+  }
+
+  @Override
+  protected Archive visitArchive(Archive archive) {
+    if (archive.sha256 == null) {
+      File file = new File(layoutFolder, archive.file);
+      try {
+        return new Archive(archive.file, HashUtils.getSHA256OfFile(file), file.length(), archive.include);
+      } catch (NoSuchAlgorithmException e) {
+        throw new RuntimeException(e);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return archive;
+  }
+}
--- cdep/src/test/java/io/cdep/cdep/fullfill/TestHashAndSizeRewritingVisitor.java
+++ cdep/src/test/java/io/cdep/cdep/fullfill/TestHashAndSizeRewritingVisitor.java
@@ -10,7 +10,10 @@ import java.io.IOException;
 
 import static com.google.common.truth.Truth.assertThat;
 
-public class TestZipFilesRewritingVisitor {
+/**
+ * Created by jomof on 4/8/2017.
+ */
+public class TestHashAndSizeRewritingVisitor {
   @Test
   public void testBasic() throws IOException {
     CDepManifestYml before = CDepManifestYmlUtils.convertStringToManifest(
@@ -20,7 +23,7 @@ public class TestZipFilesRewritingVisitor {
         .replace("${wf}", new File("../third_party/stb").getAbsolutePath())
         .visitCDepManifestYml(before);
 
-    File output = new File(".test-files/testZipFullfill").getAbsoluteFile();
+    File output = new File(".test-files/testHashAndSize").getAbsoluteFile();
 
     ZipFilesRewritingVisitor zipper = new ZipFilesRewritingVisitor(output);
     CDepManifestYml afterZipping = zipper.visitCDepManifestYml(afterSubstitution);
@@ -29,5 +32,11 @@ public class TestZipFilesRewritingVisitor {
     assertThat(new File(zipper.getLayoutFolder(), "stb_divide.h.zip").isFile()).isTrue();
     assertThat(afterZipping.interfaces.headers.file).isEqualTo("stb_divide.h.zip");
     assertThat(afterZipping.interfaces.headers.include).isEqualTo("include");
+
+    CDepManifestYml afterHashing = new HashAndSizeRewritingVisitor(zipper.getLayoutFolder())
+        .visitCDepManifestYml(afterZipping);
+
+    assertThat(afterHashing.interfaces.headers.sha256).isNotNull();
+    assertThat(afterHashing.interfaces.headers.size).isNotNull();
   }
 }
