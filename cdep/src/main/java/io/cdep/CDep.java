2290b7f19513ad0ecaab288f27eb117358e4bd58
Jomo Fisher (desktop) <jomofisher@gmail.com>
2017-04-16 21:32:37 -0700

    requires

--- cdep/src/main/java/io/cdep/CDep.java
+++ cdep/src/main/java/io/cdep/CDep.java
@@ -472,9 +472,7 @@ public class CDep {
       GeneratorEnvironment environment = getGeneratorEnvironment(false, true);
       URL remoteArchive = new URL(archive);
       File localFile = environment.getLocalDownloadFilename(coordinate, remoteArchive);
-      info("  cdep fetching %s\n", coordinate);
       GeneratorEnvironmentUtils.downloadSingleArchive(environment, coordinate, remoteArchive, size, sha256, false);
-      info("  done\n");
       require(localFile.isFile(), "Failed to download %s", localFile);
       return true;
     }
