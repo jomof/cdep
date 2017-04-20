5ded582da9002c386544e66d9bc61087311b2198
Jomo Fisher (desktop) <jomofisher@gmail.com>
2017-04-20 12:46:25 -0700

    Remove blank lines from text output

--- cdep/src/test/java/io/cdep/cdep/yml/HardnameGenerator.java
+++ cdep/src/test/java/io/cdep/cdep/yml/HardnameGenerator.java
@@ -1,17 +1,18 @@
 package io.cdep.cdep.yml;
 
+import io.cdep.cdep.Coordinate;
 import io.cdep.cdep.yml.cdepmanifest.HardNameDependency;
 import net.java.quickcheck.Generator;
 
 import static net.java.quickcheck.generator.PrimitiveGenerators.strings;
 
 public class HardnameGenerator implements Generator<HardNameDependency> {
-  Generator<String> compileGenerator = strings();
+  Generator<Coordinate> compileGenerator = new CoordinateGenerator();
   Generator<String> sha256Generator = strings();
 
   @Override
   public HardNameDependency next() {
-    String compile = compileGenerator.next();
+    String compile = compileGenerator.next().toString();
     String sha256 = sha256Generator.next();
     return new HardNameDependency(compile, sha256);
   }
