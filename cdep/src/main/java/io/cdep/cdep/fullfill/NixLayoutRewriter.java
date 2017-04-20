7cd9d1d027771b8e8cd8f93c11bb3cc7bb761883
Jomo Fisher <jomof@google.com>
2017-04-20 10:46:36 -0700

    Early exit from fullfill if there were errors

--- cdep/src/main/java/io/cdep/cdep/fullfill/NixLayoutRewriter.java
+++ cdep/src/main/java/io/cdep/cdep/fullfill/NixLayoutRewriter.java
@@ -4,6 +4,7 @@ import io.cdep.cdep.yml.cdepmanifest.CDepManifestYmlRewriter;
 
 /**
  * Translates paths like nix-layout:/path into standard paths that can be used down stream.
+ * TODO: implement this
  */
 public class NixLayoutRewriter extends CDepManifestYmlRewriter {
 
